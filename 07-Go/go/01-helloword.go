972b843e726e356a15e92741a1740ee0d3d2b576
TuringEmmy <yonglonggeng@163.com>
2019-01-01 10:21:59 +0800

    add go

---
+++ 07-Go/go/01-helloword.go
@@ -0,0 +1,12 @@
+// life is short, you need study go everyday.
+// author    TuringEmmy
+// time      2019/1/1 10:04
+// project   go
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	fmt.Println("45")
+}
---
+++ 07-Go/go/02-helloword.go
@@ -0,0 +1,11 @@
+// life is short, you need study go everyday.
+// author    TuringEmmy
+// time      2019/1/1 10:16
+// project   go
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("hello word")
+}
